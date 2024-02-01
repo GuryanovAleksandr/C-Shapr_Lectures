800d12cb43e4e0fb4981a39e6e9f10d31d3e2c06
Guryanov <sanyaguru@gmail.com>
2024-02-02 00:57:15 +0800

    add lesson2: task 1

---
+++ lesson 2/task1/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] arr = new int[5];
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 1;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[3]);
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 };
+
+int[] arr3 = { 1, 2, 3, 4, 5 };
