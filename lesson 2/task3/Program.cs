519575b53778049653c1942650d3c75e1e4d3670
Guryanov <sanyaguru@gmail.com>
2024-02-02 01:38:38 +0800

    add lesson2: task 3

--- lesson 2/task3/Program.cs
+++ lesson 2/task3/Program.cs
@@ -1,11 +1,11 @@
-﻿int n = 10;
-int[] array = {1,2,3,4,5,6,7,8,9,12};
+﻿// int n = 10;
+int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12 };
 int i = 0;
-while (i < n) //(i < arr.Length)
+while (i < array.Length)
 {
-    if(array[i] % 2 == 0)
+    if (array[i] % 2 == 0)
     {
-        Console.Write($"{array[i]} ");
+        Console.Write(array[i]);
     }
     i++;
 }
