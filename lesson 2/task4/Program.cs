de12161b5670c0415d8ab61c57e4b94921f476c7
Guryanov <sanyaguru@gmail.com>
2024-02-02 10:02:01 +0800

    add lesson2: task 4

---
+++ lesson 2/task4/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = 5;
+int[] array = { 7,2,9,5, 12 };
+int i = 0;
+int max = array[0]; 
+while (i < n)
+{
+    if ( array[i] > max)
+    {
+        max = array [i];
+    }
+    i++;
+}
+Console.WriteLine(max);
