25bae4473b62d6bd7f99c9c56fa2670dcbbc6304
Guryanov <sanyaguru@gmail.com>
2024-02-27 08:36:39 +0800

    lesson5: task1

---
+++ lesson5/task1/Program.cs
@@ -0,0 +1,24 @@
+﻿// Создадиим рекурсивную фунцию открытия матрешки
+// void OpenMatryoshka(int size)
+// {
+//     if (size == 0)
+//     {
+//         Console.WriteLine("Smallest!");
+//         return;
+//     }
+//     Console.WriteLine($"Opening matryoshka of size {size}...");
+//     OpenMatryoshka(size - 1);
+// }
+// OpenMatryoshka(5);
+
+void OpenMatryoshka(int size)
+{
+    if (size == 1)
+    {
+        Console.WriteLine("Smallest!");
+        return;
+    }
+    OpenMatryoshka(size - 1);
+    Console.WriteLine($"Opening matryoshka of size {size}...");
+}
+OpenMatryoshka(5);
