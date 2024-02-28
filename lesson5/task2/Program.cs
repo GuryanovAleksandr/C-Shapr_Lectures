128003f90405a4e6b5dc5d62efbd53d93cb61cc7
Guryanov <sanyaguru@gmail.com>
2024-02-28 16:30:48 +0800

    lesson5: task2

--- lesson5/task2/Program.cs
+++ lesson5/task2/Program.cs
@@ -1,25 +1,30 @@
-﻿// Найти факториал числа n с помощью рекурсии
-// вариант 1:
-
-// int Fact(int n)
-// {
-//     if(n == 1 || n == 0)
-//     {
-//         return 1;
-//     }
-//     return n * Fact(n-1);
-// }
-// Console.Write(Fact(10));
-
-//Вариант 2:
+﻿// // Найти факториал числа n с помощью рекурсии
+// // вариант 1:
 int Fact(int n)
 {
-    int result = 1;
-    for(int i = 2; i <= n; i++)
+    if(n == 1 || n == 0)
     {
-        result *=i;
+        Console.WriteLine($"Stop recurson: n={n}");
+        return 1;
     }
-    return result;
+    Console.WriteLine(n);
+    return Fact(n-1) * n;
 }
+Console.Write("Введите число для вычисления его факториала: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Результат:");
+Console.WriteLine(Fact(n));
 
-Console.Write(Fact(11));
+
+// //Вариант 2:
+// // int Fact(int n)
+// // {
+// //     int result = 1;
+// //     for(int i = 2; i <= n; i++)
+// //     {
+// //         result *=i;
+// //     }
+// //     return result;
+// // }
+// // Console.Write(Fact(11));
+
